5984cb4e24461fbf77a5510962bcf977324d4ef8
Rubi Kuo <rubitiara@gmail.com>
2020-01-21 14:17:04 +0100

    Cant sense input value

--- src/component/AddMovie.js
+++ src/component/AddMovie.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Helmet } from "react-helmet";
 import { Redirect } from "react-router-dom";
+import Form from "./Form";
 
 // a form for user to add a movie
 // post request
@@ -18,6 +19,19 @@ class AddMovie extends Component {
       error: false
     };
   }
+
+  handleOnchange = e => {
+    if (e.target.id === "rating") {
+      this.setState({
+        // eslint-disable-next-line no-useless-escape
+        rating: e.target.value.replace(/^(\-)*(\d+)\.(\d).*$/, "$1$2.$3")
+      });
+    } else {
+      this.setState({
+        [e.target.id]: e.target.value
+      });
+    }
+  };
 
   onSubmit = e => {
     let movie = {
@@ -43,160 +57,28 @@ class AddMovie extends Component {
         this.setState({ redirect: false, error: true });
       });
   };
+
   // to make the state property name dynamic by using object["sting"]
   // in this case [e.target.id] is a dynamic varible to state object
-  handleOnchange = e => {
-    if (e.target.id === "rating") {
-      this.setState({
-        // eslint-disable-next-line no-useless-escape
-        rating: e.target.value.replace(/^(\-)*(\d+)\.(\d).*$/, "$1$2.$3")
-      });
-    } else {
-      this.setState({
-        [e.target.id]: e.target.value
-      });
-    }
-  };
-
 
   render() {
-    let warningTitle;
-    let warningDirector;
-    let warningRating;
-    let ratingNum;
-    let warningDescription;
-
-    let title = false;
-    let director = false;
-    let rating = false;
-    let description = false;
-
-    if (this.state.title.length >= 1 && this.state.title.length <= 40) {
-      warningTitle = { color: "blue" };
-      title = true;
-    } else {
-      warningTitle = { color: "red" };
-      title = false;
-    }
-
-    if (this.state.director.length >= 1 && this.state.director.length <= 40) {
-      warningDirector = { color: "blue" };
-      director = true;
-    } else {
-      warningDirector = { color: "red" };
-      director = false;
-    }
-
-    if (this.state.rating === "") {
-      ratingNum = 0;
-    } else {
-      ratingNum = parseFloat(this.state.rating);
-    }
-
-    if (
-      parseFloat(this.state.rating) >= 0.0 &&
-      parseFloat(this.state.rating) <= 5.0
-    ) {
-      warningRating = { color: "blue" };
-      rating = true;
-    } else if (this.state.rating === undefined) {
-      warningRating = { color: "red" };
-      rating = false;
-    } else {
-      warningRating = { color: "red" };
-      rating = false;
-    }
-
-    if (
-      this.state.description.length >= 1 &&
-      this.state.description.length <= 300
-    ) {
-      warningDescription = { color: "blue" };
-      description = true;
-    } else {
-      warningDescription = { color: "red" };
-      description = false;
-    }
-
-    // to check if all the condition is true
-    let warningMsg;
-    if (title && director && rating && description) {
-      warningMsg = null;
-    } else if (this.state.error) {
-      warningMsg = <p>"Oooppps Something is wrong"</p>;
-    } else {
-      warningMsg = null;
-    }
-
+    // const { title, director, rating, description} = this.props;
     return (
       <div className="container">
         {this.state.redirect ? <Redirect to="/" /> : null}
         <Helmet>
           <title>Add Movies</title>
         </Helmet>
-        <form className="addForm" onSubmit={this.onSubmit}>
-          <div className="formWrapDiv">
-            <label htmlFor="name">
-              {" "}
-              {/*look for input field with id of Name and associate the label with input field */}
-              Title
-              <input
-                type="text"
-                id="title"
-                placeholder="title"
-                onChange={this.handleOnchange}
-              />
-              <span className="warning" style={warningTitle}>
-                {this.state.title.length}/40
-              </span>
-            </label>
-            <label htmlFor="name">
-              Director
-              <input
-                type="text"
-                id="director"
-                placeholder="director"
-                onChange={this.handleOnchange}
-              />
-              <span className="warning" style={warningDirector}>
-                {this.state.director.length}/40
-              </span>
-            </label>
-            <label htmlFor="name">
-              Rating
-              <input
-                type="number"
-                id="rating"
-                className="form-control"
-                step={0.1}
-                min={1}
-                max={5}
-                placeholder="0.0 - 5.0"
-                onChange={this.handleOnchange}
-              />
-              <span className="warning" style={warningRating}>
-                {ratingNum}/5.0
-              </span>
-            </label>
-            <label htmlFor="name">
-              Description
-              <textarea
-                type="text"
-                id="description"
-                className="descriptionCtn"
-                placeholder="description"
-                onChange={this.handleOnchange}
-              ></textarea>
-              <span className="warning" style={warningDescription}>
-                {this.state.description.length}/300
-              </span>
-            </label>
-          </div>
-          <button id="addBtn" type="submit">
-            Add
-          </button>
-        </form>
-        {warningMsg}
+
+        <Form
+          onChange={this.handleOnchange}
+          title={this.state.title}
+          director={this.state.director}
+          rating={this.state.rating}
+          description={this.state.description}
+          error = {this.state.error}
+          onSubmit={this.onSubmit}
+        />
       </div>
     );
   }
---
+++ src/component/Form.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+  
+      
+    }
+  }
+
+  
+  render() {
+    let warningTitle;
+    let warningDirector;
+    let warningRating;
+    let ratingNum;
+    let warningDescription;
+
+    let titleValidation = false;
+    let directorValidation = false;
+    let ratingValidation = false;
+    let descriptionValidation = false;
+
+    if (this.props.title.length >= 1 && this.props.title.length <= 40) {
+      warningTitle = { color: "blue" };
+      titleValidation = true;
+    } else {
+      warningTitle = { color: "red" };
+      titleValidation = false;
+    }
+
+    if (this.props.director.length >= 1 && this.props.director.length <= 40) {
+      warningDirector = { color: "blue" };
+      directorValidation = true;
+    } else {
+      warningDirector = { color: "red" };
+      directorValidation = false;
+    }
+
+    if (this.props.rating === "") {
+      ratingNum = 0;
+    } else {
+      ratingNum = parseFloat(this.props.rating);
+    }
+
+    if (
+      parseFloat(this.props.rating) >= 0.0 &&
+      parseFloat(this.props.rating) <= 5.0
+    ) {
+      warningRating = { color: "blue" };
+      ratingValidation = true;
+    } else if (this.props.rating === undefined) {
+      warningRating = { color: "red" };
+      ratingValidation = false;
+    } else {
+      warningRating = { color: "red" };
+      ratingValidation = false;
+    }
+
+    if (
+      this.props.description.length >= 1 &&
+      this.props.description.length <= 300
+    ) {
+      warningDescription = { color: "blue" };
+      descriptionValidation = true;
+    } else {
+      warningDescription = { color: "red" };
+      descriptionValidation = false;
+    }
+
+    // to check if all the condition is true
+    let warningMsg;
+    if (titleValidation && directorValidation && ratingValidation && descriptionValidation) {
+      warningMsg = null;
+    } else if (this.props.error) {
+      warningMsg = <p>"Oooppps Something is wrong"</p>;
+    } else {
+      warningMsg = null;
+    }
+
+    return (
+      <>
+        <form className="addForm" onSubmit={this.props.onSubmit}>
+          <div className="formWrapDiv">
+            <label htmlFor="name">
+              {" "}
+              {/*look for input field with id of Name and associate the label with input field */}
+              Title
+              <input
+                type="text"
+                id="title"
+          
+                placeholder="title"
+                onChange={this.props.handleOnchange}
+              />
+              <span className="warning" style={warningTitle}>
+                {this.props.title.length}/40
+              </span>
+            </label>
+            <label htmlFor="name">
+              Director
+              <input
+                type="text"
+                id="director"
+                
+                placeholder="director"
+                onChange={this.props.handleOnchange}
+              />
+              <span className="warning" style={warningDirector}>
+                {this.props.director.length}/40
+              </span>
+            </label>
+            <label htmlFor="name">
+              Rating
+              <input
+                type="number"
+                id="rating"
+                
+                className="form-control"
+                step={0.1}
+                min={1}
+                max={5}
+                placeholder="0.0 - 5.0"
+                onChange={this.props.handleOnchange}
+              />
+              <span className="warning" style={warningRating}>
+                {ratingNum}/5.0
+              </span>
+            </label>
+            <label htmlFor="name">
+              Description
+              <textarea
+                type="text"
+                id="description"
+                
+                className="descriptionCtn"
+                placeholder="description"
+                onChange={this.props.handleOnchange}
+              ></textarea>
+              <span className="warning" style={warningDescription}>
+                {this.props.description.length}/300
+              </span>
+            </label>
+          </div>
+          <button id="addBtn" type="submit">
+            Add
+          </button>
+        </form>
+        {warningMsg}
+      </>
+    );
+  }
+}
+
+export default Form;
